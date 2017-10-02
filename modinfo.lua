f259fedf4ae06951d2ea05b9df8a37cb610a1652
folknor <folk@folk.wtf>
2017-10-03 01:33:27 +0200

    Testing diplomatic addon menu with rocking graphics.

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "addonmenu",
+	name = "Addon Menu Test",
+	tags = { "Graphics" },
+	picture = false,
+	supported_version = "1.8.*",
+	remote_file_id = false,
+	readme = "readme.md",
+	exclude = {
+		"source"
+	}
+}
